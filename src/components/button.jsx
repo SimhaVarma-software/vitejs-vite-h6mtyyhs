f1dcdd735bf730fbd06999aabaa80e0365d37e92
SimhaVarma-software <simhavarma79@gmail.com>
2025-04-11 13:59:26 +0530

    Hello world

--- src/components/button.jsx
+++ src/components/button.jsx
@@ -1,1 +1,14 @@
-// write button card here
+// write button card here// write button card here
+import React from 'react'
+import './button.css';
+
+const Button = ({label}) => {
+  return (
+    <div>
+    <button className='view-btn'>
+       {label}
+        </button>
+        </div>
+  )
+}
+export default Button
--- src/components/productcard.jsx
+++ src/components/productcard.jsx
@@ -1,1 +1,21 @@
 // write product card here
+// write product card here
+import React from 'react'
+import './productcard.css';
+import Button from "./button"
+
+const productcard = () => {
+    const productImage = "https://encrypted-tbn0.gstatic.com/shopping?q=tbn:ANd9GcTnRumyZFQspqQUxclySoJCYtu0XvJxbp6BAV8aWowD_Qja6-MJXLfqWFaCbz-tW9ogBAiR7dWc8rkRXEO80f-tkx3BUSbHXQKHSXYo5NPzu-2lirFgHzPA";
+    const productName = "Amma dairilo konni pageelu";
+    const price = "₹229.90";
+
+  return (
+    <div className='product-card'>
+        <img src={productImage} alt="" className='product-image'/>
+        <h3 className='product-name'>{productName}</h3>
+        <p className='product-price'>{price}</p>
+        <Button label="View product"/>
+    </div>
+  )
+}
+export default  productcard
